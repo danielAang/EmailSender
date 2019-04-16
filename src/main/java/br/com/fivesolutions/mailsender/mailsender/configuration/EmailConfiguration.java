2d48298a2d1af4440b6bfce2618a3ac3523d7f28
Daniel Sena <danielsena04@gmail.com>
2019-04-15 23:19:52 -0300

    Ajustando conta de email para five.solutions.2019@gmail.com

--- src/main/java/br/com/fivesolutions/mailsender/mailsender/configuration/EmailConfiguration.java
+++ src/main/java/br/com/fivesolutions/mailsender/mailsender/configuration/EmailConfiguration.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -16,8 +15,8 @@ public class EmailConfiguration {
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost("smtp.gmail.com");
         sender.setPort(587);
-        sender.setUsername("danielsena04");
-        sender.setPassword("cyyszpkhntgmfyyj");
+        sender.setUsername("five.solutions.2019@gmail.com");
+        sender.setPassword("grigs45@");
         Properties props = sender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
