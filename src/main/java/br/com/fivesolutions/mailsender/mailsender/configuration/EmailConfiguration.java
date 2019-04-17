9011431e838e8aa74e49470311c6705d700ae08e
Daniel Sena <danielsena04@gmail.com>
2019-04-16 23:52:40 -0300

    Ajustando login e senha do email

--- src/main/java/br/com/fivesolutions/mailsender/mailsender/configuration/EmailConfiguration.java
+++ src/main/java/br/com/fivesolutions/mailsender/mailsender/configuration/EmailConfiguration.java
@@ -2,6 +2,7 @@ package br.com.fivesolutions.mailsender.mailsender.configuration;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class EmailConfiguration {
 
+    @Value("${spring.mail.username}")
+    private String userName;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender configureJavaMailSender() {
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost("smtp.gmail.com");
         sender.setPort(587);
-        sender.setUsername("five.solutions.2019@gmail.com");
-        sender.setPassword("grigs45@");
+        sender.setUsername(userName);
+        sender.setPassword(password);
         Properties props = sender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
